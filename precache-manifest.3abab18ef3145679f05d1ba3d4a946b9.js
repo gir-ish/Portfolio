86c8c44907e4824688d521dd5307bc3f223281d4
Girish <girishproject28@gmail.com>
2025-04-08 10:51:43 +0000

    Updates

---
+++ precache-manifest.3abab18ef3145679f05d1ba3d4a946b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edaba6c406d78f1801346a7606bbdbea",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "4b4e338452a9766d230d",
+    "url": "/Portfolio/static/css/main.269d67a2.chunk.css"
+  },
+  {
+    "revision": "5dbc90c60daf76427915",
+    "url": "/Portfolio/static/js/2.fbcd612a.chunk.js"
+  },
+  {
+    "revision": "3c341d96d1fcb9a76ddd8a8766120782",
+    "url": "/Portfolio/static/js/2.fbcd612a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa92fcaaed786764c8ae",
+    "url": "/Portfolio/static/js/3.d0d19883.chunk.js"
+  },
+  {
+    "revision": "4b4e338452a9766d230d",
+    "url": "/Portfolio/static/js/main.38440e17.chunk.js"
+  },
+  {
+    "revision": "d9dcef8ecb765f00b70c",
+    "url": "/Portfolio/static/js/runtime-main.12aafef7.js"
+  },
+  {
+    "revision": "b06ee6e3c56370010b99995b4bb04f86",
+    "url": "/Portfolio/static/media/1.b06ee6e3.jpg"
+  },
+  {
+    "revision": "559e380314c47bc112da5bc5651d5046",
+    "url": "/Portfolio/static/media/Girish_Resume.559e3803.pdf"
+  },
+  {
+    "revision": "f830d3cbe7284444da716d089149a900",
+    "url": "/Portfolio/static/media/audio-vault-enc-dec-application.drawio.f830d3cb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/about/precache-manifest.75b81e1b504b14653785cdb24dc08d42.js"
+  "/Portfolio/precache-manifest.3abab18ef3145679f05d1ba3d4a946b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/about/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
