ad1f72de276545b3ed16a14cfc9fe44f60ac19f9
Girish <girishproject28@gmail.com>
2025-04-08 10:48:22 +0000

    Updates

---
+++ precache-manifest.75b81e1b504b14653785cdb24dc08d42.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d2dd7e4b072769b7391c5ef8f17620",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "4b4e338452a9766d230d",
+    "url": "/about/static/css/main.269d67a2.chunk.css"
+  },
+  {
+    "revision": "5dbc90c60daf76427915",
+    "url": "/about/static/js/2.fbcd612a.chunk.js"
+  },
+  {
+    "revision": "3c341d96d1fcb9a76ddd8a8766120782",
+    "url": "/about/static/js/2.fbcd612a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa92fcaaed786764c8ae",
+    "url": "/about/static/js/3.d0d19883.chunk.js"
+  },
+  {
+    "revision": "4b4e338452a9766d230d",
+    "url": "/about/static/js/main.38440e17.chunk.js"
+  },
+  {
+    "revision": "6240843b28790aa41cf2",
+    "url": "/about/static/js/runtime-main.93da3a71.js"
+  },
+  {
+    "revision": "b06ee6e3c56370010b99995b4bb04f86",
+    "url": "/about/static/media/1.b06ee6e3.jpg"
+  },
+  {
+    "revision": "559e380314c47bc112da5bc5651d5046",
+    "url": "/about/static/media/Girish_Resume.559e3803.pdf"
+  },
+  {
+    "revision": "f830d3cbe7284444da716d089149a900",
+    "url": "/about/static/media/audio-vault-enc-dec-application.drawio.f830d3cb.png"
+  }
+]);
